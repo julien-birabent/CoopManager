cd1e5afc38b3ceebaf72410660171fa3e3be504c
Julien Birabent <jul.birabent@gmail.com>
2016-11-13 12:01:50 -0500

    Ajout de l'activité de login ainsi que ses fichiers ressources.

--- app/src/main/java/com/julienbirabent/coopmanager/LoginActivity.java
+++ app/src/main/java/com/julienbirabent/coopmanager/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.julienbirabent.coopmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button signInButton;
+    private EditText userName;
+    private EditText password;
+    private EditText phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getViewsById();
+        setOnClickListener();
+
+
+    }
+    private void setOnClickListener(){
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getViewsById(){
+        signInButton = (Button)findViewById(R.id.signinButton);
+        userName = (EditText) findViewById(R.id.usernameId);
+        password = (EditText) findViewById(R.id.passwordId);
+        phoneNumber = (EditText) findViewById((R.id.phoneId));
     }
 }

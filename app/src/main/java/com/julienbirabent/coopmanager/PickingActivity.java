d2869000cdd58f7e7a0f11e1b26e6eb41aab56f0
Julien Birabent <jul.birabent@gmail.com>
2016-11-14 10:37:09 -0500

    Picking activity : classe privée GetPickingList extends AsyncTask

--- app/src/main/java/com/julienbirabent/coopmanager/PickingActivity.java
+++ app/src/main/java/com/julienbirabent/coopmanager/PickingActivity.java
@@ -1,5 +1,6 @@
 package com.julienbirabent.coopmanager;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+
+import model.Book;
 
 /**
  * Created by Julien on 2016-11-13.
@@ -33,6 +36,53 @@ public class PickingActivity extends AppCompatActivity {
         searchBar = (EditText) findViewById(R.id.editText_search_picking);
 
     }
+
+    /**
+     * Remplis la liste des livres à partir d'une ArrayList<String> contenant les descrptions des
+     * livres
+     * @param books
+     */
+    public void fillBookListView(ArrayList<Book> books){
+
+        for(int i = 0; i<books.size();i++){
+
+            booksString.set(i,books.get(i).toString());
+        }
+
+        this.bookListAdapter = new ArrayAdapter<String>(this,R.layout.book_item,booksString);
+        this.getBookList().setAdapter(this.bookListAdapter);
+
+    }
+
+    private class GetPickingListTask extends AsyncTask<String,String,ArrayList<Book>>{
+
+        @Override
+        protected ArrayList<Book> doInBackground(String... params) {
+
+            ArrayList<Book> bookArrayList = new ArrayList<Book>();
+
+            // Ici faire le code pour récupérer la liste correspondant aux paramètres de la
+            // demande serveur.
+
+
+            return bookArrayList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Book> books) {
+            super.onPostExecute(books);
+
+            // après avoir récupéré la liste de livre corespondant à la recherche via le serveur, on définit
+            // cette liste comme la liste a afficher.
+            if(books!=null) {
+                fillBookListView(books);
+            }
+
+
+        }
+    }
+
+
 
     public Button getSearchButton() {
         return searchButton;
@@ -74,17 +124,7 @@ public class PickingActivity extends AppCompatActivity {
         this.booksString = booksString;
     }
 
-    /**
-     * Remplis la liste des livres à partir d'une ArrayList<String> contenant les descrptions des
-     * livres
-     * @param booksString
-     */
-    public void fillBookListView(ArrayList<String> booksString){
 
-        this.bookListAdapter = new ArrayAdapter<String>(this,R.layout.book_item,booksString);
-        this.getBookList().setAdapter(this.bookListAdapter);
-
-    }
 
 
 }

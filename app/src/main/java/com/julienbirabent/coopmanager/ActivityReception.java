aa6756d60a48cf83fe97310f012bbf639bf0c59d
Julien Birabent <jul.birabent@gmail.com>
2016-11-14 09:16:43 -0500

    Association des composants graphiques des fichiers XML aux activités

---
+++ app/src/main/java/com/julienbirabent/coopmanager/ActivityReception.java
@@ -0,0 +1,30 @@
+package com.julienbirabent.coopmanager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+/**
+ * Created by Julien on 2016-11-13.
+ */
+
+public class ActivityReception extends AppCompatActivity {
+
+    Button searchButton;
+    ListView bookList;
+    EditText searchBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reception);
+
+        searchButton = (Button) findViewById(R.id.button_search_reception);
+        bookList = (ListView) findViewById(R.id.listView_reception);
+        searchBar = (EditText) findViewById(R.id.editText_search_reception);
+
+    }
+}

f679e6681f08485b53b81fada439ed4a958f51ad
Julien Birabent <jul.birabent@gmail.com>
2016-11-21 16:17:40 -0500

    Le sessionManager contenu dans MainActivity est désormais accessible par un getter static pour toutes les autres activités.

--- app/src/main/java/com/julienbirabent/coopmanager/MainActivity.java
+++ app/src/main/java/com/julienbirabent/coopmanager/MainActivity.java
@@ -24,10 +24,7 @@ public class MainActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
+        initActivity();
     }
 
     private void initActivity(){
